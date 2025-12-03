16bb6409f9282511b04f82a273e26c7b92a353f6
Prince Laxamana <prince.laxamana116@gmail.com>
2025-12-03 14:55:50 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Siommy</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>Siommy</h1>
+    <nav>
+      <a href="index.php">Home</a> | 
+      <a href="siomai.php">Siomai</a> | 
+      <a href="about.php">About</a> | 
+      <a href="contact.php">Contact</a>
+    </nav>
